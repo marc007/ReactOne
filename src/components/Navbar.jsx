f663149b7eed49701525f09fb481e1eac30beeb2
Marc <marc007@gmail.com>
2014-11-05 19:35:04 -0800

    Edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,21 +10,26 @@ var {Link} = require('react-router');
 var Navbar = React.createClass({
   render() {
     return (
-	<nav class="navbar navbar-default" role="navigation">
-      <div className="navbar-top">
-        <div className="container">
-          <Link className="navbar-brand row" to="home">
-            <span className="glyphicon glyphicon-list-alt navbar-logo-color"></span> XL2List
-          </Link>
-		  <div className="navbar-collapse collapse">
-			<ul className="navbar-nav navbar-right">
-				<li><Link className="navbar-brand" to="login">Login</Link></li>
-				<li><Link className="navbar-brand" to="register">Register</Link></li>
-			</ul>
-		  </div>
+      <nav class="navbar navbar-default" role="navigation">
+        <div className="navbar-top">
+          <div className="container">
+            <Link className="navbar-brand row" to="home">
+              <span className="glyphicon glyphicon-list-alt navbar-logo-color"></span>
+            XL2List
+            </Link>
+            <div className="navbar-collapse collapse">
+              <ul className="navbar-nav navbar-right">
+                <li>
+                  <Link className="navbar-brand" to="login">Login</Link>
+                </li>
+                <li>
+                  <Link className="navbar-brand" to="register">Register</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-	</nav>
+      </nav>
     );
   }
 });
