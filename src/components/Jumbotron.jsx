1062672c6a1591ea69f49a35ba9e842c7db30fe5
Marc Hosy <marc007@gmail.com>
2015-01-07 23:31:25 +0000

    refactor component

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -8,20 +8,19 @@ var React = require('react');
 
 var Jumbotron = React.createClass({
   render: function() {
+    var content = '';
+    if(!this.props.islogged)
+    {
+      content = <div className="jumbotron">
+                  <div className="container text-center">
+                    <h1><span className="glyphicon glyphicon-list-alt jumbotron-logo-color"></span> {this.props.AppInfo.name}</h1>
+                    <p>{this.props.AppInfo.line}</p>
+                  </div>
+                </div>
+    }
     return (
       <div>
-        {
-          (!this.props.islogged ?
-            <div className="jumbotron">
-              <div className="container text-center">
-                <h1><span className="glyphicon glyphicon-list-alt jumbotron-logo-color"></span> {this.props.AppInfo.name}</h1>
-                <p>{this.props.AppInfo.line}</p>
-              </div>
-            </div>
-            :
-            <span></span>
-          )         
-        }
+        {content}
       </div>
     );
   }
